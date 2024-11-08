3c41e02fcfdd629f033ceee2b3d639c690f320ea
Sébastien HEIN <145024984+SebastienH422@users.noreply.github.com>
2024-11-08 14:56:30 +0100

    Importation de emp_offers_fmt.tsv

--- src/main.R
+++ src/main.R
@@ -1,1 +1,19 @@
-s
+library(data.table)
+library(stringr)
+
+# Importation des données
+# --> Création de `offres` contenant les données suivantes:
+#     entreprise, secteur, experience_requise, competences_requises, salaire, departement
+
+data = data.table(read.csv(file = "emp_offers_fmt.tsv",
+                  head = TRUE,
+                  sep = ","))
+names(data)
+# Champs: "intitule_poste" "entreprise" "type_emploi" "secteur" "experience_requise" "competences_requises""poste_desc" "salaire"              "departement"      
+
+offres = data[, c("entreprise", "secteur", # On récupère les données qui nous intéressent
+              "experience_requise", "competences_requises", "salaire", "departement")]
+
+offres_red = offres[, c("entreprise", "secteur", # On récupère les données qui nous intéressent
+              "experience_requise", "salaire", "departement")]
+
