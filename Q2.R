fa441561981eb7bb253a83af9d8bca6f7e10b085
Sébastien HEIN <sebastien.hein@icloud.com>
2024-11-08 14:55:21 +0100

    Importation en data.table

--- Q2.R
+++ Q2.R
@@ -1,26 +1,31 @@
-
-
-
 library(data.table)
 library(stringr)
 
 # Importation des données
-# --> Création de offres contenant les données suivantes:
+# --> Création de `offres` contenant les données suivantes:
 #     entreprise, secteur, experience_requise, competences_requises, salaire, departement
 
-data = read.csv(file = "emp_offers_fmt.tsv",
+data = data.table(read.csv(file = "emp_offers_fmt.tsv",
                   head = TRUE,
-                  sep = ",")
+                  sep = ","))
 names(data)
+# Champs: "intitule_poste" "entreprise" "type_emploi" "secteur" "experience_requise" "competences_requises""poste_desc" "salaire"              "departement"      
 
-offres = data[c("entreprise", "secteur", # On récupère les données qui nous intéressent
+offres = data[, c("entreprise", "secteur", # On récupère les données qui nous intéressent
               "experience_requise", "competences_requises", "salaire", "departement")]
+
+offres_red = offres[, c("entreprise", "secteur", # On récupère les données qui nous intéressent
+              "experience_requise", "salaire", "departement")]
+
+
+
+
 
 
 # Récupération des compétences
 # --> Création de competences contenant chaque compétences utilisées dans le champs 
 #     competences_requises. Les doublons sont supprimés.
-
+head(offres)
 separateur = function(x){ # Fonction qui sera appliquée sur 
   str_split(string = x, # chaque élément de la colonnes competences_requises
             pattern = ",")
