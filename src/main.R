809c22bf0e0cd57e822b1d4349668506efa39c61
Sébastien HEIN <145024984+SebastienH422@users.noreply.github.com>
2024-11-08 15:07:32 +0100

    Update main.R

--- src/main.R
+++ src/main.R
@@ -1,7 +1,7 @@
 library(data.table)
 library(stringr)
 
-# Importation des données
+# 2.4 : Importation des données offres d'emploi
 # --> Création de `offres` contenant les données suivantes:
 #     entreprise, secteur, experience_requise, competences_requises, salaire, departement
 
@@ -9,11 +9,8 @@ data = data.table(read.csv(file = "emp_offers_fmt.tsv",
                   head = TRUE,
                   sep = ","))
 names(data)
-# Champs: "intitule_poste" "entreprise" "type_emploi" "secteur" "experience_requise" "competences_requises""poste_desc" "salaire"              "departement"      
+# Champs: "intitule_poste" "entreprise" "type_emploi" "secteur" "experience_requise" "competences_requises""poste_desc" "salaire" "departement"      
 
 offres = data[, c("entreprise", "secteur", # On récupère les données qui nous intéressent
               "experience_requise", "competences_requises", "salaire", "departement")]
-
-offres_red = offres[, c("entreprise", "secteur", # On récupère les données qui nous intéressent
-              "experience_requise", "salaire", "departement")]
 
