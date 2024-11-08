5a82f603fab5fb7b84ac064e973cf462d00a3d39
Sébastien HEIN <145024984+SebastienH422@users.noreply.github.com>
2024-11-08 22:47:16 +0100

    Création de base_emp + correction importation

--- smams_emploi.R
+++ smams_emploi.R
@@ -10,7 +10,7 @@ library(stringr)
 # --> Création de `offres` contenant les données suivantes:
 #     entreprise, secteur, experience_requise, competences_requises, salaire, departement
 #
-data = data.table(read.csv(file = "emp_offers_fmt.tsv", # Importation
+data = data.table(read.csv(file = "DATA/emp_offers_fmt.tsv", # Importation
                   head = TRUE,
                   sep = ","))
 names(data) # Récupération des champs
@@ -20,7 +20,9 @@ offres = data[, c("entreprise", "secteur", # Création de 'offres'
 
 
 # Création de la data.table 'base_emp'
-#
+# Champs: id_firm firm_name n_offres sector_main avg_req_exp top_skill_req avg_wage addre_dept_main
+# 
 base_emp = data.table(id_firm = character(0), firm_name = character(0), n_offres = integer(0), 
                       sector_main = character(0), avg_req_exp = numeric(0), top_skill_req = character(0), 
                       avg_wage = numeric(0), addre_dept_main = character(0))
+
