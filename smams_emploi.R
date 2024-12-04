10b249c264a00f3c7a73d07df6b6465d27d2317a
Sébastien HEIN <145024984+SebastienH422@users.noreply.github.com>
2024-12-04 15:52:54 +0100

    TODO propre

--- smams_emploi.R
+++ smams_emploi.R
@@ -31,18 +31,32 @@ base_emp$addre_dept_main = base_emp$departement
 
 # TODO
 # Fonctions pour construire les colonnes de base_emp
-#     Transformer l'expérience requise et les salaires en nombre, 
-#     Scinder les compétences et les secteurs
+#     get_exp_req: Retourner l'expérience requise en liste de string contenant les expériences
+#     get_wage: Transformer les salaires en nombre, 
+#               faire la moyenne lorsqu'il s'agit d'une fourchette, 
+#               retourner le salaire
+#     get_skills: Scinder le string des compétences en liste de string des compétences
+#                 retourner la liste de string des compétences
+#     get_sector: Scinder le string des secteurs en liste de string des secteurs
+#                 retourner la liste de string des secteurs
 # 
 # Fonctions à appliquer pour l'aggregation
-#    firm_name: Choisir un nom au hasard (le premier)
-#    n_offres: Sommer les valeurs (toutes initialisées à 1)
-#    // sector_name: Prendre celui ou ceux qui apparaissent le plus souvent
-#                 ou Fusionner les différents secteurs
-#    avg_req_exp: Moyenne des valeurs
-#    top_skill_req: Prendre les compétences qui apparraissent le plus souvent
-#    avg_wage: Moyenne des valeurs
-#    // addre_dept_main: Prendre celui qui apparait le plus souvent
+#     set_firm_name: IN: Liste des noms (string)
+#                    OUT: Le premier nom, ou celui qui revient le plus souvent
+#     set_sector_name: IN: Liste de listes de secteurs (string, même format que le retour de get_sector)
+#                          n: nombre de secteurs voulus à la fin
+#                      OUT: String des n secteurs qui reviennent le plus souvent
+#                      TODO: Fusionner les listes, 
+#                            trier par ordre d'apparition, 
+#                            prendre les n valeurs qui apparraissent le plus souvent
+#     set_top_skill_req: IN: Liste de liste des compétences (string, même format que le retour de get_skills)
+#                            n: nombre de compétences voulus à la fin
+#                        OUT: String des n compétences qui reviennent le plus souvent
+#                        TODO: Fusionner les listes, 
+#                              trier par ordre d'apparition, 
+#                              prendre les n valeurs qui apparraissent le plus souvent
+#     set_addre_dept_main: IN: Liste de int des départements
+#                          OUT: int du département qui revient le plus souvent
 #
 # Aggregation:
 # base_emp <- base_emp[{bool pour sélectionner les données},
@@ -50,4 +64,3 @@ base_emp$addre_dept_main = base_emp$departement
 #                           par exemple sum(n_offers),
 #                           séparées par des virgules},
 #                      by = {identifiant, ici id_firm}]
-
