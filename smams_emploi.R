f4df9c427c43c1bb8bdf6ee5eaaa86b5d9288e23
Sébastien HEIN <145024984+SebastienH422@users.noreply.github.com>
2024-12-04 14:54:51 +0100

    TODO + Fonction id_firm

--- smams_emploi.R
+++ smams_emploi.R
@@ -18,11 +18,36 @@ names(data) # Récupération des champs
 offres = data[, c("entreprise", "secteur", # Création de 'offres'
               "experience_requise", "competences_requises", "salaire", "departement")]
 
+base_emp$id_firm = apply(X = base_emp,
+                         MARGIN = 1,
+                         FUN = get_id_firm)
+base_emp$firm_name = base_emp$entreprise
+base_emp$n_offres = rep(1, dim(base_emp)[1])
+base_emp$sector_name = base_emp$secteur
+base_emp$avg_req_exp 
+base_emp$top_skill_req
+base_emp$avg_wage
+base_emp$addre_dept_main = base_emp$departement
 
-# Création de la data.table 'base_emp'
-# Champs: id_firm firm_name n_offres sector_main avg_req_exp top_skill_req avg_wage addre_dept_main
+# TODO
+# Fonctions pour construire les colonnes de base_emp
+#     Transformer l'expérience requise et les salaires en nombre, 
+#     Scinder les compétences et les secteurs
 # 
-base_emp = data.table(id_firm = character(0), firm_name = character(0), n_offres = integer(0), 
-                      sector_main = character(0), avg_req_exp = numeric(0), top_skill_req = character(0), 
-                      avg_wage = numeric(0), addre_dept_main = character(0))
+# Fonctions à appliquer pour l'aggregation
+#    firm_name: Choisir un nom au hasard (le premier)
+#    n_offres: Sommer les valeurs (toutes initialisées à 1)
+#    // sector_name: Prendre celui ou ceux qui apparaissent le plus souvent
+#                 ou Fusionner les différents secteurs
+#    avg_req_exp: Moyenne des valeurs
+#    top_skill_req: Prendre les compétences qui apparraissent le plus souvent
+#    avg_wage: Moyenne des valeurs
+#    // addre_dept_main: Prendre celui qui apparait le plus souvent
+#
+# Aggregation:
+# base_emp <- base_emp[{bool pour sélectionner les données},
+#                      {fonctions pour aggréger les données, 
+#                           par exemple sum(n_offers),
+#                           séparées par des virgules},
+#                      by = {identifiant, ici id_firm}]
 
--- smams_src_utilities.R
+++ smams_src_utilities.R
@@ -7,3 +7,12 @@
 # Fonctions Matilin (del)
 
 # Fonctions Seb (del)
+get_id_firm = function(line) {
+  if (!is.null(line['entreprise'])) {
+    name = line['entreprise'] # Nom de l'entreprise
+    name_norm = stringi::stri_trans_general(tolower(name), "Latin-ASCII") # Sans accent ni majuscule
+    return (substring(name_norm, 1, min(4, nchar(name_norm)))) # 4 premiers caractères
+  } else {
+    stop("La colonne 'entreprise' est manquante dans l'objet 'line'")
+  }
+}
